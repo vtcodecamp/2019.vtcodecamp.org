8f4136eaac59bee15ac9363fb1bf87760dab67cd
Kyle Mitofsky <Kyle.Mitofsky@vermont.gov>
2019-04-25 15:09:46 -0400

    inline and minify ctritical styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
+const CleanCSS = require("clean-css");
+
+
 module.exports = function(eleventyConfig) {
 
     // Copy the `assets/` directory (css, images, etc)
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
+    // add cssmin filter
+    eleventyConfig.addFilter("cssmin", function(code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
 
     return {
         dir: {
