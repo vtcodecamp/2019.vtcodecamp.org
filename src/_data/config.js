016220710d09d9159095a1e714fdaaca44fce41d
Zeke Farwell <ezekielf@gmail.com>
2019-05-04 11:11:35 -0400

    test netlify environment variables

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,7 +1,27 @@
 
-require('dotenv').config()
+// require('dotenv').config()
 
+
+const CONTEXT = process.env.CONTEXT;
+const URL = process.env.URL;
+const DEPLOY_PRIME_URL = process.env.DEPLOY_PRIME_URL;
+const DEFAULT_DEV_URL = 'http://localhost:8080';
+
+  
 module.exports = {
-  environment: process.env.ENVIRONMENT,
-  domain: process.env.DOMAIN,
+    buildContext: (CONTEXT) ? CONTEXT : 'development',
+    baseUrl: getBaseUrl(),
+    baseUrlCanonical: (URL) ? URL : DEFAULT_DEV_URL,
 };
+
+
+function getBaseUrl()
+{
+    if (CONTEXT == 'production') {
+        return URL;
+    } else if (DEPLOY_PRIME_URL) {
+        return DEPLOY_PRIME_URL;
+    } else {
+        return DEFAULT_DEV_URL;
+    }
+}
